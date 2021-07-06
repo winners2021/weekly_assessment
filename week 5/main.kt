19a23796b3a1acd10492e37aa2c99439091f7707
winners2021 <85246675+winners2021@users.noreply.github.com>
2021-07-06 16:40:48 +0100

    Create main.kt

---
+++ week 5/main.kt
@@ -0,0 +1,21 @@
+fun vowel(s: String): String {
+    val re = StringBuilder()
+    for (c in s){
+        if (c!='A' && c !='a'
+            && c != 'E' && c !='e'
+            && c != 'I' && c != 'i'
+            && c != 'O' && c != 'o'
+            && c != 'U' && c != 'u') {
+                re.append(c)
+
+                }
+
+                }
+
+       return re.toString()
+
+       }
+
+fun main() {
+    println (vowel("First Programming Language"))
+}
